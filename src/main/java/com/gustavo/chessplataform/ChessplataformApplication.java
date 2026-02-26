ee84f7611134c0b2fce9cfacc33cb67529b39a25
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2026-02-25 22:09:23 -0300

    initial setup

---
+++ src/main/java/com/gustavo/chessplataform/ChessplataformApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.chessplataform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessplataformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessplataformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/chessplataform/ChessplataformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.chessplataform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessplataformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
