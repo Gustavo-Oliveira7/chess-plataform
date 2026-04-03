c656a75a900f69c9d2a94ca18e1f34d46966aeed
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2026-04-03 11:06:24 -0300

    add Game controller endpoints

---
+++ src/main/java/com/gustavo/chessplataform/controller/GameController.java
@@ -0,0 +1,43 @@
+package com.gustavo.chessplataform.controller;
+
+import com.gustavo.chessplataform.domain.model.Game;
+import com.gustavo.chessplataform.service.GameService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/games")
+public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Game createGame(@RequestParam Long whitePlayerID) {
+        return gameService.createGame(whitePlayerID);
+    }
+
+    @PostMapping("/{gameId}/join")
+    public Game joinGame(
+            @PathVariable Long gameId,
+            @RequestParam Long blackPlayerId
+    ) {
+        return gameService.joinGame(gameId, blackPlayerId);
+    }
+
+    @GetMapping("/{id}")
+    public Game getGame(@PathVariable Long id) {
+        return gameService.getGame(id);
+    }
+
+    @GetMapping
+    public List<Game> listGames() {
+        return gameService.listGames();
+    }
+}
